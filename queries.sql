72543c5452c5838bcffa146143063aa099cafd90
alok463 <alokzyx@gmail.com>
2020-10-24 16:39:30 +0530

    added queries

--- queries.sql
+++ queries.sql
@@ -4,3 +4,7 @@
 SELECT username, post_id, user_id, comment_body, comment_created_at from user_comments
 INNER JOIN user_posts  ON user_posts.id = user_comments.post_id
  INNER JOIN users ON users.id = user_comments.user_id where post_id=3;
+
+
+ //query to delete all comments
+ DELETE from user_comments where id=1;
